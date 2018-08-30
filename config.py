c5dc981ce68975245f8c2915ce5aac4279f4db4b
Nikolay <nchernov@algotradinghub.com>
2018-08-30 12:39:53 +0300

    config moved into separate module

---
+++ config.py
@@ -0,0 +1,31 @@
+import json
+from gmusicapi import Mobileclient
+CONFIG_FILE_NAME = 'gmusic2mp3.json'
+
+def print_all_my_devices(api: Mobileclient):
+    for device in api.get_registered_devices():
+        print('{')
+        for k in device:
+            print('\t{}: {}'.format(k, device[k]))
+        print('}')
+
+def generate_config():
+    CONFIG = {
+        'login': 'music_fun@gmail.com',
+        'password': 'Bu!He8ePH0',
+        'device_id': '39c5fb55f84a1d9a',
+        'root_dir': r'C:\Users\nikol\GMusic',
+        'albums': {
+            'Несчастный случай': 'Чернослив и курага',
+            'System Of A Down': 'Mezmerize'
+        }
+    }
+    with open(CONFIG_FILE_NAME, 'w', encoding='utf8') as f:
+        json.dump(CONFIG, f, indent=2, ensure_ascii=False)
+
+
+if not os.path.exists(CONFIG_FILE_NAME):
+    generate_config()
+    raise AssertionError('Cannot find config file. But, for example, generated the new one; please edit it :)')
+
+CONFIG = json.load(open(CONFIG_FILE_NAME, 'r'))
--- gmusic2mp3.py
+++ gmusic2mp3.py
@@ -1,39 +1,9 @@
 from gmusicapi import Mobileclient
 import wget
 import os
-import json
-
-CONFIG_FILE_NAME = 'gmusic2mp3.json'
-
-def print_all_my_devices(api: Mobileclient):
-    for device in api.get_registered_devices():
-        print('{')
-        for k in device:
-            print('\t{}: {}'.format(k, device[k]))
-        print('}')
-
-def generate_config():
-    CONFIG = {
-                'login': 'nchernov@gmail.com',
-                'password': 'Bu!He8ePH0',
-                'device_id': '39c5fb55f84a1d9a',
-                'root_dir': r'C:\Users\nikol\GMusic',
-                'albums': {
-                    'Несчастный случай': 'Чернослив и курага',
-                    'System Of A Down': 'Mezmerize'
-                }
-             }
-    with open(CONFIG_FILE_NAME, 'w', encoding='utf8') as f:
-        json.dump(CONFIG, f, indent=2, ensure_ascii=False)
-
+from config import CONFIG
 
 def main():
-    CONFIG = None
-    if not os.path.exists(CONFIG_FILE_NAME):
-        generate_config()
-        raise AssertionError('Cannot find config file. But, for example, generated the new one; please edit it :)')
-    with open(CONFIG_FILE_NAME, 'r') as f:
-        CONFIG = json.load(open(CONFIG_FILE_NAME))
     api = Mobileclient()
     if api.login(CONFIG['login'], CONFIG['password'], CONFIG['device_id']):
         if not api.is_subscribed:
@@ -57,7 +27,7 @@ def main():
                             if not os.path.exists(full_path):
                                 wget.download(artRef['url'], full_path)
                     url = api.get_stream_url(tr['id'])
-                    file_name = '{:02d} {}.mp3'.format( tr['trackNumber'], tr['title'])
+                    file_name = '{:02d} {}.mp3'.format(tr['trackNumber'], tr['title'])
                     file_name = file_name.replace('/', '-')
                     full_path = os.path.join(dir_name, file_name)
                     if not os.path.exists(full_path):
