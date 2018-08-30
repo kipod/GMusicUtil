f89c477a3872569754083b4ff4ecb7588fa6c74c
Nikolay <nchernov@algotradinghub.com>
2018-08-30 12:25:58 +0300

    download cover.jpg

--- gmusic2mp3.py
+++ gmusic2mp3.py
@@ -49,6 +49,13 @@ def main():
                 if not os.path.exists(dir_name):
                     os.makedirs(dir_name)
                 for tr in tracks:
+                    if 'albumArtRef' in tr:
+                        i = 0
+                        for artRef in tr['albumArtRef']:
+                            img_file_name = 'cover.{}.jpg' if i else 'cover.jpg'
+                            full_path = os.path.join(dir_name, img_file_name)
+                            if not os.path.exists(full_path):
+                                wget.download(artRef['url'], full_path)
                     url = api.get_stream_url(tr['id'])
                     file_name = '{:02d} {}.mp3'.format( tr['trackNumber'], tr['title'])
                     file_name = file_name.replace('/', '-')
